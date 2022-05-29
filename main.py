627c0b63dfa6cc6cf355e588d594c818d2c92370
kintsnok <kintsnok@yandex.ru>
2022-05-29 23:54:50 +0300

    Task 2

--- main.py
+++ main.py
@@ -40,11 +40,45 @@ class SuperHeroAPI():
         
         return smartest
 
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Accept': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path: str, file_name):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        href = self._get_upload_link(disk_file_path=f'Netology/{file_name}').get('href', '')
+        response = requests.put(href, data=open(file_path + f'{file_name}', 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Success')
 
 
 if __name__ == '__main__':
+
+    # Задание 1
     shapi = SuperHeroAPI()
     heros_list = ['Hulk', 'Captain America', 'Thanos']
     smartest_hero = shapi.get_smartest(heros_list)
     pprint(smartest_hero)
 
+
+    # Задание 2
+    path_to_file = 'D:/'
+    file_name = 'some_awesome_file.txt'
+    uploader = YaUploader(YA_POLIGON_TOKEN)
+    result = uploader.upload(path_to_file, file_name)
+    
